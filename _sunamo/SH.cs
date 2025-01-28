2a94d515f3e306cdd1d1eb51d0890f8b2779705b
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:34 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/SH.cs
+++ _sunamo/SH.cs
@@ -1,21 +1,4 @@
 namespace SunamoGitConfig._sunamo;
 internal class SH
 {
-    internal static List<string> GetLines(string text)
-    {
-        List<string> result = [];
-        using (StringReader sr = new StringReader(text))
-        {
-            string? line;
-            while ((line = sr.ReadLine()) != null)
-            {
-                result.Add(line);
-            }
-        }
-        return result;
-    }
-    internal static string JoinNL(List<string> lines)
-    {
-        return string.Join(Environment.NewLine, lines);
-    }
 }
